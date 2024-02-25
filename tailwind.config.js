26c386b067feb2c999fbdf7b314f37a605f381f9
Christopher <Chris1Bobsy@Gmail.com>
2024-02-25 22:34:40 +0100

    add: styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,jsx}"],
+  safelist: ["linesPattern", "rhombusPattern", "checkeredPattern"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainBg: "var(--main-bg)",
+        mainWhite: "var(--main-white)",
+        lightGray: "var(--light-gray)",
+        lightMidGray: "var(--light-mid-gray)",
+        midGray: "var(--mid-gray)",
+        darkGray: "var(--dark-gray)",
+        switchChecked: "var(--switch-checked)",
+        switchUnchecked: "var(--switch-unchecked)",
+        tileBasic: "var(--tile-basic)",
+        tileHint: "var(--tile-hint)",
+        tileHover: "var(--tile-hover)",
+        deepIndigo: "var(--deep-indigo)",
+        fadedIndigo: "var(--faded-indigo)",
+        taskButtonHoverIcon: "var(--task-button-hover-icon)",
+      },
+    },
   },
   plugins: [],
 };
